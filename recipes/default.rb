c3dee92c1f1b7740140a03f4ed6406d8ea0dc73c
Ryutaro YOSHIBA <ryuzee@gmail.com>
2013-07-21 10:44:27 +0900

    add iptables settings

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,3 +12,14 @@ when "redhat", "centos", "fedora"
 end
 include_recipe "openvpn::default"
 include_recipe "openvpn::users"
+
+case node[:platform]
+when "redhat", "centos", "fedora", "amazon"
+  execute "iptables -t nat -A POSTROUTING -s #{node["openvpn"]["subnet"]}/#{node["openvpn"]["netmask"]} -o eth0 -j MASQUERADE" do
+    action :run
+  end
+
+  execute "/etc/rc.d/init.d/iptables save" do
+    action :run
+  end
+end
